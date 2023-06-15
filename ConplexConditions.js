c1dee49e938d986100defeda89a8070e1df39621
Rakibul Shahar <78203754+Shaharislam@users.noreply.github.com>
2023-06-15 17:25:32 +0600

    Complex Statement Add

---
+++ ConplexConditions.js
@@ -0,0 +1,18 @@
+const car = {
+    price: 2000,
+    color: "red",
+    numDoors: 5
+};
+
+if (car.price < 2000 && car.color === "red" && car.numDoors === 4) {
+    console.log("We'll take it!");
+} else {
+    console.log("Welp, it's best to keep looking");
+}
+
+
+if (car.price < 2000 && car.color === "red" || car.numDoors === 4) {
+    console.log("We'll take it!");
+} else {
+    console.log("Welp, it's best to keep looking");
+}
